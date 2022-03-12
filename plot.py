7171abf5e46f62e39c06ab35475d07759bcea7e9
Mukta13 <mukta@instance-1.c.pongatari.internal>
2022-03-12 06:23:34 +0000

    1 million plottt

--- plot.py
+++ plot.py
@@ -3,20 +3,21 @@ import pickle
 import numpy as np
 
 
-with (open("losses_file.pkl", "rb")) as openfile1:
-    data1 = pickle.load(openfile1)
-frames, losses = zip(*data1)
+#with (open("losses_file.pkl", "rb")) as openfile1:
+data1 = pickle.load(open('losses_file.pkl', 'rb'))
+frames1, losses = zip(*data1)
 
-plt.scatter(frames, losses)
+plt.scatter(frames1, losses)
 plt.xlabel('# of frames')
 plt.ylabel('losses')
 plt.show()
 plt.savefig('losses1.png')
 
-with (open("all_rewards_file.pkl", "rb")) as openfile2:
-    data2 = pickle.load(openfile2)
-frames, rewards = zip(*data2)
-plt.scatter(frames, rewards)
+#with (open("all_rewards_file.pkl", "rb")) as openfile2:
+   # data2 = pickle.load(openfile2)
+data2 = pickle.load(open('all_rewards_file.pkl', 'rb'))
+frames2, rewards = zip(*data2)
+plt.scatter(frames2, rewards)
 plt.xlabel('# of frames')
 plt.ylabel('rewards')
 plt.show()
